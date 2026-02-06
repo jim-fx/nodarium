74c2978cd16d2dd95ce1ae8019dfb9098e52b4b6
Max Richter <max.richter@dynabase.de>
2026-02-06 16:16:07 +0100

    chore: cleanup git.json a bit

--- .gitea/scripts/build.sh
+++ .gitea/scripts/build.sh
@@ -9,7 +9,6 @@ cp CHANGELOG.md app/static/CHANGELOG.md
 REF_TYPE="${GITHUB_REF_TYPE:-branch}"
 REF_NAME="${GITHUB_REF_NAME:-$(basename "$GITHUB_REF")}"
 BRANCH="${GITHUB_HEAD_REF:-}"
-BASE_BRANCH="${GITHUB_BASE_REF:-}"
 if [[ -z "$BRANCH" && "$REF_TYPE" == "branch" ]]; then
   BRANCH="$REF_NAME"
 fi
@@ -21,14 +20,12 @@ cat >app/static/git.json <<EOF
   "ref_type": "${REF_TYPE}",
   "sha": "${GITHUB_SHA:-}",
   "run_number": "${GITHUB_RUN_NUMBER:-}",
-  "server_url": "${GITHUB_SERVER_URL:-}",
   "event_name": "${GITHUB_EVENT_NAME:-}",
   "workflow": "${GITHUB_WORKFLOW:-}",
   "job": "${GITHUB_JOB:-}",
   "commit_message": "$(git log -1 --pretty=%B)",
   "commit_timestamp": "$(git log -1 --pretty=%cI)",
-  "branch": "${BRANCH}",
-  "base_branch": "${BASE_BRANCH}"
+  "branch": "${BRANCH}"
 }
 EOF
 
